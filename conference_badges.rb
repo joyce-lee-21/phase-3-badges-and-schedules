94e0133fe414f0ae02127ebf23075bca6f9efeb1
Joyce Lee <joycelee901@gmail.com>
2021-06-28 12:29:18 -0500

    commit tree 98cdf332073c5eefccb8066c92f8c9dbe87aceec

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,19 @@
 # Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+    array.map{ |name| badge_maker(name)}
+end
+
+def assign_rooms(attendees)
+    # attendees.with_index(1)
+    # room_assignments = attendees.with_index(1)
+    attendees.map.with_index(1){ |attendee, i| "Hello, #{attendee}! You'll be assigned to room #{i}!"}
+end
+
+def printer(attendees)
+    batch_badge_creator(attendees).each{|attendee| puts attendee}
+    assign_rooms(attendees).each{|attendee| puts attendee}
+end
